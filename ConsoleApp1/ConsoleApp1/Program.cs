e0130c2ac91141a4a67ae3b3409e59c9a7f703af
Soloduha <33834254+Soloduha@users.noreply.github.com>
2018-05-14 20:11:19 +0300

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Jaja Quelho"); 
+            Console.WriteLine("I love C#"); 
         }
     }
 }
